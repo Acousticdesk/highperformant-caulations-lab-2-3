f20273b55b4dec1febfad0d161f52384f2fd04bf
Andrii Kicha <akicha@star.global>
2022-10-20 16:13:50 +0300

    squash

--- app.ts
+++ app.ts
@@ -1,33 +1,34 @@
-import * as createError from 'http-errors';
-import * as express from 'express'
-import { ErrorRequestHandler } from 'express';
-import * as logger from 'morgan'
+import * as createError from "http-errors";
+import * as express from "express";
+import { ErrorRequestHandler } from "express";
+import * as logger from "morgan";
 
-import indexRouter from './routes';
-import HttpError from './errors';
+import indexRouter from "./routes";
+import HttpError from "./errors";
 
 const app = express();
 
-app.use(logger('dev'));
+// register express middleware
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+// routes
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err: HttpError, req, res) {
+// generic error handler
+app.use(function (err: HttpError, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.json({
-    status: err.status || 500
+    status: err.status || 500,
   });
 } as ErrorRequestHandler);
 
